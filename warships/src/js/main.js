f5829a438506fe0caeb7c7c59334b2681d90fd47
borbely-dominik-peter <98750586+borbely-dominik-peter@users.noreply.github.com>
2025-03-25 20:30:41 +0000

    Update main.js

--- warships/src/js/main.js
+++ warships/src/js/main.js
@@ -1,1 +1,18 @@
 // code goes brrr
+
+const ROUTINGTARGET = document.querySelector("#root");
+
+const NavLinks = document.querySelectorAll("a[data-href]");
+
+const RoutingTable = {
+    "/" : {page: "start.html", code: null},
+    "/country" : {page: "country.html", code: null}
+}
+
+const LoadJSON = () => {
+    
+}
+
+const NavClickEvent = () => {
+
+}
