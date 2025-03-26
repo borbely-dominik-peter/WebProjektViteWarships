c5d2a8421a8a4ae8ccd8615263f9a1086b5fab1b
borbely-dominik-peter <98750586+borbely-dominik-peter@users.noreply.github.com>
2025-03-26 16:49:09 +0000

    Update main.js

--- warships/src/js/main.js
+++ warships/src/js/main.js
@@ -1,18 +1,52 @@
 // code goes brrr
 
+import Start from "./start.js";
+
 const ROUTINGTARGET = document.querySelector("#root");
+
+const PAGESFOLDER = "/src/html/"
 
 const NavLinks = document.querySelectorAll("a[data-href]");
 
 const RoutingTable = {
-    "/" : {page: "start.html", code: null},
-    "/country" : {page: "country.html", code: null}
+    "/" : {page: "start.html", code: Start},
+    "/all" : {page: "all.html", code: null}
 }
 
-const LoadJSON = () => {
-    
+const LoadPage = async (page) => {
+    const resp = await fetch(PAGESFOLDER+page);
+    const convresp = await resp.text();
+    return convresp;
 }
 
-const NavClickEvent = () => {
-
+const NavClickEvent = async (event) => {
+    event.preventDefault();
+    let page = event.target.dataset.href;
+    let data = await LoadPage(RoutingTable[page].page);
+    DynCode(RoutingTable[page].code)
+    window.history.pushState({},"",page)
+    ROUTINGTARGET.innerHTML = data;
 }
+
+const DynCode = (code) => {
+    if (code != null) {
+        let DynamicCode = eval(code);
+        new DynamicCode();
+    }
+}
+
+window.addEventListener("popstate", async () => {
+    const data = await LoadPage(RoutingTable[window.location.pathname].page);
+    DynCode(RoutingTable[window.location.pathname].code);
+    ROUTINGTARGET.innerHTML = data;
+})
+
+window.addEventListener("load", async () => {
+    const data = await LoadPage(RoutingTable[window.location.pathname].page);
+    DynCode(RoutingTable[window.location.pathname].code);
+    ROUTINGTARGET.innerHTML = data;
+})
+
+NavLinks.forEach(NavI => {
+    NavI.addEventListener("click", NavClickEvent)
+})
